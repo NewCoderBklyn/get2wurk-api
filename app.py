1fc5805f323c05caf0f1166674b8b0c07f67821d
NewCoderBklyn <alexkagen@gmail.com>
2025-10-18 16:51:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, HTTPException, Depends, Header
+from fastapi import FastAPI, HTTPException, Depends, Header, Security
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
 
